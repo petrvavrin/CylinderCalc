7168a8aa23e4adec42de5d057159cf67e2b27e0d
petrvavrin <148798764+petrvavrin@users.noreply.github.com>
2024-02-09 12:59:17 +0100

    Update CylinderCalc.py

--- CylinderCalc.py
+++ CylinderCalc.py
@@ -4,7 +4,7 @@ def cylinder_volume():
     return round(math.pi*radius*radius*height, 2)
 
 def cylinder_surface():
-    return round(2*math.pi*radius*radius+height ,2)
+    return round((2*math.pi)*radius*(radius+height) ,2)
 
 choice = input('What you want to calculate?\n1.Cylinder volume\n2.Cylinder surface\n')
 
@@ -26,4 +26,6 @@ elif choice == '2':
     print('You chose surface')
     height = input('Enter h - Height of the cylinder\n')
     radius = input('Enter r - Radius of the base\n')
-    #cylinder_surface()
+    height = int(height)
+    radius = int(radius)
+    print(f'Result is: {cylinder_surface()}')
